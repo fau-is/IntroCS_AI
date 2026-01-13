b76e15985d0f11a39b5eb363e19faeb0696af50e
Workstation3 <weixin.w.wang@fau.de>
2026-01-13 12:54:08 +0100

    add rm

--- __init__.py
+++ __init__.py
@@ -60,8 +60,7 @@ def functionality_check_delete():
 @check50.check()
 def functionality_check_file():
     """combined functionality"""
-    check50.exists("tasks.txt")
-    check50.exists("audit.txt")
+    check50.run("rm -f tasks.txt audit.txt")
     try:
         check50.run("python3 tracker.py add Task_A").exit(0)
         #check50.run("python3 tracker.py add Task_B").exit(0)

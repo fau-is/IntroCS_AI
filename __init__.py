d6dbda8aa39d66ad941caedc460b208f3464fd0e
Workstation3 <weixin.w.wang@fau.de>
2026-01-13 15:29:12 +0100

    fix

--- __init__.py
+++ __init__.py
@@ -152,6 +152,12 @@ def functionality_check_file():
     if len(events) != 3:
         raise check50.Failure("tasks.txt contains wrong number of task entries.")
 
+    if events[1]["status"] != "1" :
+        raise check50.Failure("tasks1 has wrong status")
+
+    if  events[0]["id"] != "1":
+        raise check50.Failure("tasks0 has wrong id")
+
     if events[1]["status"] != "1" or events[0]["id"] != "1" or events[2]["name"] != "Task_D":
         raise check50.Failure("tasks.txt has wrong entries")
 

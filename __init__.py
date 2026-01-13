8d1d3ce1072f87a99c54b5b2a2e1b3dab30ffbae
Workstation3 <weixin.w.wang@fau.de>
2026-01-13 12:32:47 +0100

    change check

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ def run_tracker():
 
 
 @check50.check()
-def functionality_check():
+def functionality_check_add():
     """combined functionality"""
     #check50.run("rm -f tasks.txt audit.txt").exit(0)
     try:
@@ -29,29 +29,43 @@ def functionality_check():
     except:
         raise check50.Failure("Cannot run finish operation sequence with command add")
 
+@check50.check()
+def functionality_check_finish():
+    """combined functionality"""
     try:
+        check50.run("python3 tracker.py add Task_A").exit(0)
+        check50.run("python3 tracker.py add Task_B").exit(0)
         check50.run("python3 tracker.py finish 2").exit(0)
     except:
         raise check50.Failure("Cannot run finish operation sequence with command finish")
 
+@check50.check()
+def functionality_check_delete():
+    """combined functionality"""
     try:
+        check50.run("python3 tracker.py add Task_A").exit(0)
+        check50.run("python3 tracker.py add Task_B").exit(0)
+        check50.run("python3 tracker.py finish 2").exit(0)
         check50.run("python3 tracker.py add Task_C").exit(0)
         check50.run("python3 tracker.py add Task_D").exit(0)
-    except:
-        raise check50.Failure("Cannot run finish operation sequence with command add")
-
-    try:
         check50.run("python3 tracker.py delete 3").exit(0)
     except Exception as e:
-        raise check50.Failure(str(e)) #"Cannot run finish operation sequence with command delete")
+        raise check50.Failure("Cannot run finish operation sequence with command delete")
 
     try:
         check50.run("python3 tracker.py list").exit(0)
     except:
         raise check50.Failure("Cannot run finish operation sequence with command list")
 
-
+def functionality_check_file():
+    """combined functionality"""
     try:
+        check50.run("python3 tracker.py add Task_A").exit(0)
+        check50.run("python3 tracker.py add Task_B").exit(0)
+        check50.run("python3 tracker.py finish 2").exit(0)
+        check50.run("python3 tracker.py add Task_C").exit(0)
+        check50.run("python3 tracker.py add Task_D").exit(0)
+        check50.run("python3 tracker.py delete 3").exit(0)
         check50.run("diff -u example.txt tasks.txt").exit(0)
     except:
         raise check50.Failure("The output of tasks.txt does not follow the expected format")

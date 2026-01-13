edfe4f40dcfd5673bb5b92f8064daa5b08ad7ea5
Workstation3 <weixin.w.wang@fau.de>
2026-01-13 13:03:08 +0100

    check all test

--- __init__.py
+++ __init__.py
@@ -18,14 +18,8 @@ def run_tracker():
     check50.exists("tasks.txt")
     check50.exists("audit.txt")
 
+
 @check50.check()
-def run_tracker_file():
-    '''check file without running tracker.py '''
-    check50.exists("tasks.txt")
-    check50.exists("audit.txt")
-
-
-#@check50.check()
 def functionality_check_add():
     """combined functionality"""
     #check50.run("rm -f tasks.txt audit.txt").exit(0)
@@ -35,7 +29,8 @@ def functionality_check_add():
     except:
         raise check50.Failure("Cannot run finish operation sequence with command add")
 
-#@check50.check()
+
+@check50.check()
 def functionality_check_finish():
     """combined functionality"""
     try:
@@ -45,7 +40,8 @@ def functionality_check_finish():
     except:
         raise check50.Failure("Cannot run finish operation sequence with command finish")
 
-#@check50.check()
+
+@check50.check()
 def functionality_check_delete():
     """combined functionality"""
     try:
@@ -54,7 +50,7 @@ def functionality_check_delete():
         check50.run("python3 tracker.py finish 2").exit(0)
         check50.run("python3 tracker.py add Task_C").exit(0)
         check50.run("python3 tracker.py add Task_D").exit(0)
-        #check50.run("python3 tracker.py delete 3").exit(0)
+        check50.run("python3 tracker.py delete 3").exit(0)
     except Exception as e:
         raise check50.Failure("Cannot run finish operation sequence with command delete")
 
@@ -63,12 +59,10 @@ def functionality_check_delete():
     except:
         raise check50.Failure("Cannot run finish operation sequence with command list")
 
+
 @check50.check()
 def functionality_check_file():
     """combined functionality"""
-    check50.run("rm -f tasks.txt audit.txt")
-    check50.exists("tasks.txt")
-    check50.exists("audit.txt")
     try:
         check50.run("python3 tracker.py add Task_A").exit(0)
         check50.run("python3 tracker.py add Task_B").exit(0)
